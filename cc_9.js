028c98afcab3163a931d7a9164df12b1cb237267
Hient <hient@usf.edu>
2025-02-22 23:00:44 -0500

    Task 3 - Created Company Class

--- cc_9.js
+++ cc_9.js
@@ -38,3 +38,26 @@ class Manager extends Employee { //Creating class Mangager extending from Employ
 const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
 console.log(mgr1.getDetails()); // Expected output: "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
 console.log(mgr1.calculateBonus()); // Expected output: 9600
+
+//Task 3: Creating a Company Class
+class Company {
+    constructor (name){ //Constructor setting up specified properties
+        this.name = name;
+        this.employees = []; //Initializing empty employees array
+    }
+    addEmployee(employee){ //Setting up addEmployee method
+        this.employees.push(employee) //.push() employee to the employees array
+    }
+    listEmployees(){ //Setting up listEmployees method
+        this.employees.forEach(emp =>console.log(emp.getDetails())); //console.log the getDetails method forEach employee
+    }
+}
+
+//Test Cases
+const company = new Company("TechCorp");
+company.addEmployee(emp1);
+company.addEmployee(mgr1);
+company.listEmployees();
+// Expected output:
+// "Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
+// "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
